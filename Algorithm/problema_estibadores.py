f4bd97aa42d007d11e29c30ae172a65f64fe5f75
Álvaro Fernández García <alvfergar@gmail.com>
2016-05-19 13:53:11 +0200

    Acciones finalizadas aunq hay que revisarlas porque puede haber problema cuando la asignación no es de solo un conjunto sino cuando la union de dos conjuntos. Algunos cambios en summary porque faltaba una variable de estado

--- Algorithm/problema_estibadores.py
+++ Algorithm/problema_estibadores.py
@@ -4,9 +4,10 @@ import numpy as np
 
 # Clases de símbolos de objetos
 Localizaciones = ['l1', 'l2', 'l3']
-Robots = ['r1', 'r2']
+Robots = ['r1']
 Gruas = ['g1', 'g2']
 Contenedores = ['c1', 'c2', 'c3']
+ContenedoresYSuelo = Contenedores + ['suelo']
 
 # Variables de estados
 
@@ -25,23 +26,26 @@ localizacion_contenedor = probpl.VariableDeEstados(
 
 contenedor_encima = probpl.VariableDeEstados(
     nombre='contenedor_encima({b})',
-    b=Contenedores
+    b=ContenedoresYSuelo
 )
 
-localizacion_contenedor = probpl.VariableDeEstados(
-    nombre='localizacion_contenedor({b})',
+localizacion_robot = probpl.VariableDeEstados(
+    nombre='localizacion_robot({b})',
     b=Robots
 )
+
+# Ejemplo impresion de Variable de Estado
+print(contenedor_encima({'c1': 'Ninguno'}))
 
 ## Esto se puede sacar a una clase que contruya una matriz
 ## tal como se hace aqui, con sus get y set
 traceabilityMatrix = np.zeros((len(Localizaciones), len(Localizaciones)), dtype='bool')
-#l1 adyacente l2
+# l1 adyacente l2
 traceabilityMatrix[0][1] = True
-#l2 adyacente l3
+# l2 adyacente l3
 traceabilityMatrix[1][2] = True
 
-matrizTrazSimet = (traceabilityMatrix+traceabilityMatrix.T)
+matrizTrazSimet = (traceabilityMatrix + traceabilityMatrix.T)
 
 print(matrizTrazSimet)
 
@@ -55,10 +59,9 @@ adyacente = probpl.RelaciónRígida(lambda x, y:
 adyacente_fun = lambda x, y: matrizTrazSimet[
     Localizaciones.index(x),
     Localizaciones.index(y)]
-print('¿Existe relacion de adyacencia?', adyacente_fun('l1','l3'))
+print('¿Existe relacion de adyacencia?', adyacente_fun('l1', 'l3'))
 
-
-radio = {'g1': ['l1','l2'],'g2':['l3']}
+radio = {'g1': ['l1', 'l2'], 'g2': ['l3']}
 
 radio_accion = probpl.RelaciónRígida(lambda grua, localizacion:
                                      localizacion in radio[grua])
@@ -66,7 +69,145 @@ radio_accion = probpl.RelaciónRígida(lambda grua, localizacion:
 ##Prueba de la función lambda
 radio_fun = lambda grua, localizacion: localizacion in radio[grua]
 
-print('¿Esta en el radio de accion?', radio_fun('g1','l3'))
+print('¿Esta en el radio de accion?', radio_fun('g1', 'l3'))
+
+
 
 
 # Operadores
+
+desplazar_robot_contenedor = probpl.Operador(
+    nombre='desplazar_robot_contenedor({l1},{l2},{r},{c})',
+    precondiciones=[localizacion_robot({'{r}': '{l1}'}),
+                    localizacion_contenedor({'{c}': '{l1}'})],
+    efectos=[localizacion_robot({'{r}': '{l2}'}),
+             localizacion_contenedor({'{c}': '{l2}'})],
+    relaciones_rígidas=adyacente('{l1}', '{l2}'),
+    l1=Localizaciones,
+    l2=Localizaciones,
+    r=Robots,
+    c=Contenedores
+)
+
+desplazar_robot = probpl.Operador(
+    nombre='desplazar_robot({l1},{l2},{r})',
+    precondiciones=[localizacion_robot({'{r}': '{l1}'})],
+    efectos=[localizacion_robot({'{r}': '{l2}'})],
+    relaciones_rígidas=adyacente('{l1}', '{l2}'),
+    l1=Localizaciones,
+    l2=Localizaciones,
+    r=Robots
+)
+
+coger_contenedor_suelo = probpl.Operador(
+    nombre='coger_contenedor_suelo({c},{l},{g})',
+    precondiciones=[contenedor_cogido({'{g}': 'ninguno'}),
+                    posicion_contenedor({'{c}': 'suelo'}),
+                    localizacion_contenedor({'{c}': '{l}'})],
+    efectos=[contenedor_cogido({'{g}': '{c}'}),
+             posicion_contenedor({'{c}': '{g}'})],
+    relaciones_rígidas=radio_accion('{g}', '{l}'),
+    c=Contenedores,
+    l=Localizaciones,
+    g=Gruas
+)
+
+coger_contenedor_pila = probpl.Operador(
+    nombre='coger_contenedor_pila',
+    precondiciones=[contenedor_cogido({'{g}': 'ninguno'}),
+                    posicion_contenedor({'{c1}': '{c2}'}),
+                    contenedor_encima({'{c1}': 'ninguno'}),
+                    localizacion_contenedor({'{c1}': '{l}'}),
+                    localizacion_contenedor({'{c2}': '{l}'})],
+    efectos=[contenedor_cogido({'{g}': '{c1}'}),
+             posicion_contenedor({'{c1}': '{g}'}),
+             contenedor_encima({'{c2}': 'ninguno'})],
+    relaciones_rígidas=radio_accion('{g}', '{l}'),
+    c1=Contenedores,
+    c2=Contenedores,
+    l=Localizaciones,
+    g=Gruas
+)
+
+coger_contenedor_robot = probpl.Operador(
+    nombre='coger_contenedor_robot',
+    precondiciones=[localizacion_contenedor({'{c}': '{l}'}),
+                    contenedor_cogido({'{g}': 'ninguno'}),
+                    contenedor_cogido({'{r}': '{c}'}),
+                    localizacion_robot({'{r}': '{l}'})],
+    efectos=[contenedor_cogido({'{g}': '{c}'}),
+             contenedor_cogido({'{r}': 'ninguno'})],
+    relaciones_rígidas=radio_accion('{g}', '{l}'),
+    c=Contenedores,
+    g=Gruas,
+    l=Localizaciones,
+    r=Robots
+)
+
+poner_contenedor_suelo = probpl.Operador(
+    nombre='poner_contenedor_suelo',
+    precondiciones=[contenedor_cogido({'{g}': '{c}'}),
+                    contenedor_encima({'suelo': 'ninguno'})],
+    efectos=[contenedor_cogido({'{g}': 'ninguno'}),
+             contenedor_encima({'suelo': '{c}'})],
+    relaciones_rígidas=radio_accion('{g}', '{l}'),
+    c=Contenedores,
+    g=Gruas,
+    l=Localizaciones
+)
+
+poner_contenedor_pila = probpl.Operador(
+    nombre='poner_contenedor_pila',
+    precondiciones=[contenedor_cogido({'{g}': '{c1}'}),
+                    posicion_contenedor({'{c1}': '{g}'}),
+                    localizacion_contenedor({'{c2}': '{l}'}),
+                    contenedor_encima({'{c2}': 'ninguno'})],
+    efectos=[contenedor_cogido({'{g}': 'ninguno'}),
+             contenedor_encima({'{c2}': '{c1}'}),
+             posicion_contenedor({'{c1}': '{c2}'}),
+             contenedor_encima({'{c2}': 'ninguno'})
+             ],
+    relaciones_rígidas=radio_accion('{g}', '{l}'),
+    c1=Contenedores,
+    c2=Contenedores,
+    g=Gruas,
+    l=Localizaciones
+)
+
+
+poner_contenedor_robot = probpl.Operador(
+    nombre='poner_contenedor_robot',
+    precondiciones=[contenedor_cogido({'{g}': '{c}'}),
+                    contenedor_cogido({'{r}': 'ninguno'}),
+                    localizacion_robot({'{r}': '{l}'})],
+    efectos=[contenedor_cogido({'{g}': 'ninguno'}),
+             contenedor_cogido({'{r}': '{c}'}),
+             localizacion_contenedor({'{c}': '{l}'})],
+    relaciones_rígidas=radio_accion('{g}', '{l}'),
+    c1=Contenedores,
+    g=Gruas,
+    l=Localizaciones,
+    r=Robots
+)
+
+#
+# problema_mundo_bloques = probpl.ProblemaPlanificación(
+#     operadores=[desplazar_robot_contenedor,
+#                 desplazar_robot,
+#                 coger_contenedor_suelo,
+#                 coger_contenedor_pila,
+#                 coger_contenedor_robot,
+#                 poner_contenedor_suelo,
+#                 poner_contenedor_pila,
+#                 poner_contenedor_robot],
+#     estado_inicial=probpl.Estado(posición({'A': 'mesa',
+#                                            'B': 'mesa',
+#                                            'C': 'B'}),
+#                                  bloque_encima({'A': 'ninguno',
+#                                                 'B': 'C',
+#                                                 'C': 'ninguno'}),
+#                                  bloque_cogido('ninguno')),
+#     objetivos=posición({'A': 'B',
+#                         'B': 'C',
+#                         'C': 'mesa'})
+# )

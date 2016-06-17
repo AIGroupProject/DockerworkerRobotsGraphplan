9f7d80c8a362f70d12056f943d1ba47ca48f39ea
Álvaro Fernández García <alvfergar@gmail.com>
2016-06-17 10:34:22 +0200

    estructura de accion

--- util/accion.py
+++ util/accion.py
@@ -1,36 +1,20 @@
 class Accion(object):
-    def __init__(self, nombre, precondiciones, efectosN, efectosP):
+    def __init__(self, nombre, precondiciones, efectosp, efectosn):
         self.nombre = nombre
         self.precondiciones = precondiciones
-        self.efectosN = efectosN
-        self.efectosP = efectosP
+        self.efectosp = efectosp
+        self.efectosn = efectosn
 
 
-    def getNombre(self):
+    def get_nombre(self):
         return self.nombre
 
-    def getPrecondiciones(self):
+    def get_precondiciones(self):
         return self.precondiciones
 
-    def getEfectosN(self):
-        return self.efectosN
+    def get_efectosp(self):
+        return self.efectosp
 
-    def getEfectosP(self):
-        return self.efectosP
+    def get_efectosn(self):
+        return self.efectosn
 
-    def siEsPrecondicion(self, atomo):
-        return atomo in self.precondiciones
-
-    def siEsEfectoN(self, atomo):
-        return atomo in self.efectosN
-
-    def siEsEfectoP(self, atomo):
-        return atomo in self.efectosP
-
-    def siPrecondEnLista(self,lista):
-
-        return [False if pred not in lista else True for pred in self.precondiciones]
-        # for pred in self.precondiciones:
-        #     if pred not in lista:
-        #         return False
-        # return True

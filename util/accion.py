34ec024473cc5a3356e7b7f3cb0d4de093c641ae
Álvaro Fernández García <alvfergar@gmail.com>
2016-06-17 11:57:51 +0200

    actualizado el tipo accion para que sea compatible

--- util/accion.py
+++ util/accion.py
@@ -1,38 +1,38 @@
 class Accion(object):
-    def __init__(self, nombre, precondiciones, efectosN, efectosP):
+    def __init__(self, nombre, precondiciones, efectosp, efectosn):
         self.nombre = nombre
         self.precondiciones = precondiciones
-        self.efectosN = efectosN
-        self.efectosP = efectosP
+        self.efectosp = efectosp
+        self.efectosn = efectosn
 
 
-    def getNombre(self):
+    def get_nombre(self):
         return self.nombre
 
-    def getPrecondiciones(self):
+    def get_precondiciones(self):
         return self.precondiciones
 
-    def getEfectosN(self):
+    def get_efectosn(self):
         return self.efectosN
 
-    def getEfectosP(self):
+    def get_efectosp(self):
         return self.efectosP
 
-    def siEsPrecondicion(self, atomo):
-        return atomo in self.precondiciones
-
-    def siEsEfectoN(self, atomo):
-        return atomo in self.efectosN
-
-    def siEsEfectoP(self, atomo):
-        return atomo in self.efectosP
-
-    def siPrecondEnLista(self,lista):
-
-        return [False if pred not in lista else True for pred in self.precondiciones]
-        # for pred in self.precondiciones:
-        #     if pred not in lista:
-        #         return False
-        # return True
+    # def siEsPrecondicion(self, atomo):
+    #     return atomo in self.precondiciones
+    #
+    # def siEsEfectoN(self, atomo):
+    #     return atomo in self.efectosN
+    #
+    # def siEsEfectoP(self, atomo):
+    #     return atomo in self.efectosP
+    #
+    # def siPrecondEnLista(self,lista):
+    #
+    #     return [False if pred not in lista else True for pred in self.precondiciones]
+    #     # for pred in self.precondiciones:
+    #     #     if pred not in lista:
+    #     #         return False
+    #     # return True
     def es_aplicable(self,lista):
         return all(pred in lista for pred in  self.precondiciones)
